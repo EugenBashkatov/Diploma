dca408b7eda514b56681d439409cf157e6400aa6
EugenBashkatov <39494078+EugenBashkatov@users.noreply.github.com>
2021-02-02 01:51:05 +0300

    for Johnnik 2021.01.31 Supernova in one pass

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import numpy as np
 import xlsxwriter
 
 
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
