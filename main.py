409619524197f19fd168cf7afef169be8b7aa7ec
EugenBashkatov <39494078+EugenBashkatov@users.noreply.github.com>
2021-02-02 08:34:10 +0300

    for Johnnik 2021.01.31 Supernova in one pass+Clusters show

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # This is a sample Python script.
 
+Token:
+cd5c0f2b8bb97f0cd4e46dd6f0e5647922a163d3
+
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import pandas as pd
